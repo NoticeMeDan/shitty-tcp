63510af0ece951f0ed9f31ace8a805db040a55b2
Vilfred <Vilfredsikker@gmail.com>
2018-09-05 19:39:58 +0200

    QuestionableDatagram setup

--- src/main/java/com/noticemedan/shittytcp/QuestionableDatagramSocket.java
+++ src/main/java/com/noticemedan/shittytcp/QuestionableDatagramSocket.java
@@ -3,5 +3,26 @@ package com.noticemedan.shittytcp;
 public class QuestionableDatagramSocket {
     public static void main(String[] args) {
         System.out.println("God i'm questionable");
+
+
+    }
+
+    public void executeRandomDatagram(){
+        int random = (int) (Math.random() * 4);
+
+        switch (random){
+            case(0): // Discard
+
+                break;
+            case(1): // Create duplicates
+
+                break;
+            case(2): // Simply send the datagram
+
+                break;
+            case(3): // Reorders the datagram
+
+                break;
+        }
     }
 }
