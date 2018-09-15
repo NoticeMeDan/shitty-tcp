30e22a941ad26e37fb81a528ebf9ffbf45aa9548
alol <alol@itu.dk>
2018-09-15 12:56:43 +0200

    Finished Estimator.java functionality, now only needs to print statistics.

--- src/main/java/com/noticemedan/shittytcp/exercise2/Estimator.java
+++ src/main/java/com/noticemedan/shittytcp/exercise2/Estimator.java
@@ -2,118 +2,132 @@ package com.noticemedan.shittytcp.exercise2;
 
 import com.noticemedan.shittytcp.exercise1.QuestionableDatagramSocket;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.util.Arrays;
-import java.util.Random;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.*;
 
 
 public class Estimator {
 
-    private int numOfLost,
-                numOfDuplicated,
-                numOfReordered;
-    private int ammount;
+    private int datagramSize, numOfDatagrams;
 
-    private QuestionableDatagramSocket socket;
+    private ArrayList<DatagramPacket> send;
+    private LinkedList<DatagramPacket> sendList;
+    private ArrayList<DatagramPacket> received;
 
-    public Estimator(int datagramSize, int numOfDatagrams, int transInterval){
-        if(datagramSize > 60.000){
-            throw new IllegalArgumentException("Please provide a datagram size less than 60.000");
-        }
-        this.ammount = numOfDatagrams;
+
+    private QuestionableDatagramSocket sendSocket;
+    private DatagramSocket receiveSocket;
+
+    public Estimator(int datagramSize, int numOfDatagrams){
+        this.datagramSize = datagramSize > 60000 ? 60000 : datagramSize;
+        this.numOfDatagrams = numOfDatagrams;
+        this.received = new ArrayList<>();
         try {
-            this.sendRandomDatagrams(datagramSize, numOfDatagrams, transInterval);
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
+            this.sendSocket = new QuestionableDatagramSocket();
+            this.receiveSocket = new DatagramSocket(7007);
+        } catch (SocketException e) {
             e.printStackTrace();
         }
     }
 
-    //Main method for sending random datagrams
-    private void sendRandomDatagrams(int size, int num, int interval) throws IOException, InterruptedException {
-        //The host of our RFC862 server
-        InetAddress aHost = InetAddress.getByName("localhost");
-        //The last-sent packet stored and used to identify reordered packets
-        DatagramPacket prevPacket;
 
-        //Making sure we can read System.out from our RFC862 server;
-        PrintStream oldOut = System.out;
-        ByteArrayOutputStream pipeOut = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(pipeOut));
+    public static void estimate(int datagramSize, int numOfDatagrams, int interval) {
+        Estimator est = new Estimator(datagramSize, numOfDatagrams);
+        est.createRandomDatagrams();
 
-        //Runs the loop #num amount of times, sending and receiving answers, recording drops, dupes and reorders.
-        for(int i = 0; i<num; i++) {
-            //Create random packet as 'request'
-            QuestionableDatagramSocket aSocket = new QuestionableDatagramSocket();
-            byte[] m = generateRandomByteArray(size);
-            DatagramPacket request = new DatagramPacket(m, num, aHost, 7007);
-            prevPacket = request;
-            //send request
-            aSocket.send(request);
-
-            //Checks what our console has written
-            String output = new String(pipeOut.toByteArray());
-            //This is the expected string RFC862 Sends if it is succesfully sent
-            String expectedResult = "RECIEVED: " + Arrays.toString(request.getData());
-
-            if(output.equals(expectedResult)){
-                //All is fine, do nothing.
+        Runnable sender = () -> {
+            try {
+                est.sendSocket.send(est.sendList.pop());
+            } catch (Exception e) {
+                System.out.println("No more elements in list!");
             }
-            //TODO: Check whether or not this actually checks for duplicate messages
-            else if(output.equals(expectedResult + expectedResult)){
-                this.numOfDuplicated++;
-            }//TODO: Check whether or not this actually checks for previous messages
-            else if(output.equals("RECIEVED: " + Arrays.toString(prevPacket.getData()))){
-                this.numOfReordered++;
+        };
+
+        Runnable receiver = () -> {
+            while(true) {
+                try {
+                    byte[] buffer = new byte[est.datagramSize];
+                    DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+                    est.receiveSocket.receive(reply);
+                    est.received.add(reply);
+                } catch (SocketTimeoutException e) {
+                    System.out.println("Socket timeout. Closing...");
+                    break;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-            else {
-                //Otherwise we've dropped the message, probably.
-                this.numOfLost++;
-            }
-            //Close the socket, and start again after #interval amount of time.
-            aSocket.close();
-            Thread.sleep(interval);
+        };
+
+        //Open the receiver thread and start listening for Packets on the DatagramSocket.
+        Thread receiveThread = new Thread(receiver);
+        receiveThread.start();
+
+        //Start a Thread with an Executor Service that runs the packet-sending method with the given interval
+        ScheduledExecutorService sendScheduler = Executors.newScheduledThreadPool(1);
+        ScheduledFuture handler = sendScheduler.scheduleAtFixedRate(sender, interval, interval, TimeUnit.MILLISECONDS);
+        sendScheduler.schedule(() -> { handler.cancel(true);
+                                        est.printResults();
+                                       try {
+                                           receiveThread.join();   //Close the receive thread.
+                                       } catch (InterruptedException e) { e.printStackTrace(); }
+
+                                      }, interval * est.send.size(), TimeUnit.MILLISECONDS);
+
+
+    }
+
+    private void printResults() {
+        for (DatagramPacket msg: send) {
+            System.out.println(new String(msg.getData()));
+        }
+        System.out.println("---------------");
+        for (DatagramPacket msg: received) {
+            System.out.println(new String(msg.getData()));
         }
 
-        //Remember to set the System.Out back to where it was!
-        System.setOut(oldOut);
-        System.out.println("All done!");
+        //System.out.println("######## STATISTICS ########");
+        //System.out.println("Duplicates: " + duplicatePackets);
+        //System.out.println("Reordered: " + reorderedPackets);
+        //System.out.println("Discarded: " + discardedPackets);
+        //System.out.println("Packets sent: " + packetsSend);
+        //System.out.println("Packets received: " + packetsReceived);
+    }
 
+    private void createRandomDatagrams() {
+        InetAddress aHost = null;
+        try {
+            aHost = InetAddress.getByName("localhost");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        this.send = new ArrayList<>();
+        this.sendList = new LinkedList<>();
+
+        if(this.datagramSize > 60000){
+            throw new IllegalArgumentException("Please provide a datagram size less than 60.000");
+        }
+        for(int i = 0; i<this.numOfDatagrams; i++) {
+            byte[] m = generateRandomByteArray(this.datagramSize);
+            DatagramPacket packet = new DatagramPacket(m,this.numOfDatagrams, aHost, 7007);
+            this.send.add(packet);
+            this.sendList.add(packet);
+        }
     }
 
     private byte[] generateRandomByteArray(int s){
-        Random r = new Random();
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < s; i++) {
-            char c = (char)(r.nextInt((int)(Character.MAX_VALUE)));
+            char c = (char)((int) ThreadLocalRandom.current().nextInt(50, 110 + 1));
             sb.append(c);
         }
         return sb.toString().getBytes();
     }
 
-    public int getNumOfLost() {
-        return numOfLost;
+    public static void main(String args[]) {
+        estimate(20,20,100);
     }
 
-    public int getNumOfDuplicated() {
-        return numOfDuplicated;
-    }
-
-    public int getNumOfReordered() {
-        return numOfReordered;
-    }
-
-    public String getPercOfLost() {
-        return ammount/numOfLost + "%";
-    }
-
-    public String getPercOfDuplicated() {
-        return ammount/numOfDuplicated + "%";
-    }
 }
